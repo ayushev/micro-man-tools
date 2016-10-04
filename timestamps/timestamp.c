2d3a85d48028edf6d5b48b1b847701368e1306c9
Andreas Walz <andreas.walz@hs-offenburg.de>
2016-10-04 14:47:50 +0200

    added some comments and license

--- timestamps/timestamp.c
+++ timestamps/timestamp.c
@@ -1,17 +1,49 @@
+/*
+ * MICRO-MAN-TOOLS: A set of tools for embedded system development 
+ * Copyright (C) 2016 Andreas Walz
+ *
+ * Author: Andreas Walz (andreas.walz@hs-offenburg.de)
+ *
+ * This file is part of THE-MAN-TOOLS.
+ *
+ * THE-MAN-TOOLS are free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * THE-MAN-TOOLS are distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with THE-MAN-TOOLS; if not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 #include "timestamp.h"
 
 #ifndef TIMESTAMP_N_MAX
 #define TIMESTAMP_N_MAX 128
 #endif
 
-static uint_fast16_t	timestamp_n = 0;
-static uint32_t			timestamp_buf[TIMESTAMP_N_MAX];
 
-extern void		timestamp_send_byte(uint8_t byte);
-extern uint32_t	timestamp_get_ticks(void);
+/* the counter to hold the current number of time stamps in the buffer */
+static uint_fast16_t timestamp_n = 0;
+
+/* the buffer to hold time stamps between being set and being set out */
+static uint32_t timestamp_buf[TIMESTAMP_N_MAX];
+
+/* externally defined function to send out one byte */
+extern void timestamp_send_byte(uint8_t byte);
+
+/* externally defined function to return current tick counter */
+extern uint32_t timestamp_get_ticks(void);
 
 
 /*
+ * Function to set a time stamp, i.e. write a time stamp to the buffer
  * ___________________________________________________________________________
  */
 void timestamp_set(uint_fast8_t tag) {
@@ -30,6 +62,7 @@ void timestamp_set(uint_fast8_t tag) {
 
 
 /*
+ * Function to send out all time stamps from the buffer and clear the buffer
  * ___________________________________________________________________________
  */
 void timestamp_flush(void) {
@@ -39,20 +72,29 @@ void timestamp_flush(void) {
 	uint_fast16_t	i;
 	uint_fast8_t	j;
 
+    /* array used to convert data to hex */
 	static const uint8_t timestamp_hex[16] = {
 			'0', '1', '2', '3',	'4', '5', '6', '7',
 			'8', '9', 'A', 'B',	'C', 'D', 'E', 'F' };
 
+    /* iterate over all time stamps in the buffer */
 	for (i = 0; i < timestamp_n; i++) {
+
+        /* load time stamp from buffer ... */
 		stamp = timestamp_buf[i];
+
+        /* ... and process it (nibble by nibble) */
 		for (j = 8; j > 0; j--) {
 			byte = timestamp_hex[(stamp & 0xF0000000) >> 28];
 			stamp = stamp << 4;
 			timestamp_send_byte(byte);
 		}
+
+        /* send out newline */
 		timestamp_send_byte('\r');
 		timestamp_send_byte('\n');
 	}
 
+    /* clear the buffer */
 	timestamp_n = 0;
 }
